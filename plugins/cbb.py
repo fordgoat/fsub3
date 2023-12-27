1d1d52e6fd3b11e58d01e2634ea6d581ca84ad92
urangunang <145663184+urangunang@users.noreply.github.com>
2023-12-27 17:35:16 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,24 +1,56 @@
 # (©)Codexbotz
-# Recode By @mahadappa
-# Kalo clone Gak usah hapus ya kontol
-
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+# Recode by @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
 
 from bot import Bot
-from config import CHANNEL, GROUP, OWNER
+from config import OWNER
+from REA import Data
+from pyrogram import filters
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, Message
+
+
+@Bot.on_message(filters.private & filters.incoming & filters.command("order"))
+async def _about(client: Bot, msg: Message):
+    await client.send_message(
+        msg.chat.id,
+        Data.ORDER.format(client.username, OWNER),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(Data.mbuttons),
+    )
+
+
+@Bot.on_message(filters.private & filters.incoming & filters.command("help"))
+async def _help(client: Bot, msg: Message):
+    await client.send_message(
+        msg.chat.id,
+        "<b>Cara Menggunakan Bot ini</b>\n" + Data.HELP,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(Data.buttons),
+    )
 
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "about":
-        await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Channel : @mahadappa\n • Owner Repo : @xxdappansa< • Link Repo: <a href='https://www.xnxx.com/search/big%20tits%20porn'>Klik Disini</a></b>\n",
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(" ᴛᴜᴛᴜᴘ ", callback_data="close")]]
-            ),
-        )
+    if data == "order":
+        try:
+            await query.message.edit_text(
+                text=Data.ORDER.format(client.username, OWNER),
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(Data.mbuttons),
+            )
+        except MessageNotModified:
+            pass
+    elif data == "help":
+        try:
+            await query.message.edit_text(
+                text="<b>Cara Menggunakan Bot ini</b>\n" + Data.HELP,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(Data.buttons),
+            )
+        except MessageNotModified:
+            pass
     elif data == "close":
         await query.message.delete()
         try:
